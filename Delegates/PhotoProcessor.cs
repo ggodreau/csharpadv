be5da4eeb1daba369b6253fa590ab83f25ff24cd
ggodreau <godreau@gmail.com>
2018-06-26 11:21:33 -0500

    2_7 working photoprocessor with stacked delegate chain

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Delegates
+{
+    class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+
+        // the second argument into Process()
+        // indicates where to delegate TO
+        public void Process(string path, PhotoFilterHandler photoFilterHandler)
+        {
+            var photo = new Photo();
+            photo.Load(path);
+           
+            photoFilterHandler(photo);
+            photo.Save();
+
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var photoProcessor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            // tricky part; you must assign the method you want to apply
+            // to the DELEGATE function
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            // you can treat the delegate function as a list of method objects
+            // ...i.e. a stack of things to do, and you push successive methods
+            // onto it
+            filterHandler += filters.ApplyContrast;
+
+            // this is of type: Delegates.PhotoProcessor + PhotoFilterHandler
+            Console.WriteLine(filterHandler.GetType());
+            // target is: Delegates.PhotoFilters
+            Console.WriteLine(filterHandler.Target);
+            // method is: Void ApplyContrast(Delegates.Photo)
+            Console.WriteLine(filterHandler.Method);
+            photoProcessor.Process("c:\\photo.jpg", filterHandler);
+        }
+    }
+}
