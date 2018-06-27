6b03bd3e810b99b45c2d7b9595685814212863be
ggodreau <godreau@gmail.com>
2018-06-27 12:15:25 -0500

    2_8 futzing with simple squared program

---
+++ LambdaExpressions/BookRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class BookRepository
+    {
+    }
+}
---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        public static int _n = 5;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().Squared(_n).ToString());
+        }
+
+        public int Squared(int n)
+        {
+            return n * n;
+        }
+    }
+}
