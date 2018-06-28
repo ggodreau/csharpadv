deed194a3d5357943b2859701ce6ad21070c61e8
ggodreau <godreau@gmail.com>
2018-06-28 16:18:40 -0500

    2_10 extension method vanilla

---
+++ ExtensionMethods/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            // do shat
+            return "hello buttface";
+        }
+    }
+}
