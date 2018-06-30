31d0495f28aea4b719a04294b04d9a3991220312
ggodreau <godreau@gmail.com>
2018-06-30 11:34:28 -0500

    2_12 nullable value types, GetValueOrDefault() HasValue() Value()

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // can't set value types to null; the below won't work
+            // DateTime birthday = null;
+
+            // have to use Nullable as below
+            System.Nullable<DateTime> nullday = null;
+            Console.WriteLine("nullday = " + nullday);
+
+            // or the shorthand is to use a question mark following the datatype
+            DateTime? birthday = null;
+            Console.WriteLine("birthday = " + birthday);
+
+            // lets get the values
+            Console.WriteLine("getvalordef = " + birthday.GetValueOrDefault());
+            Console.WriteLine("hasval? = " + birthday.HasValue);
+            // below will return a null reference exception error
+            // Console.WriteLine("val = " + birthday.Value);
+        }
+    }
+}
