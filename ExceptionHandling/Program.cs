4b7cd3a786487d63ff65034aa22b7d8cf7d528d2
ggodreau <godreau@gmail.com>
2018-07-02 10:54:45 -0500

    2_14 calculator try/catch 2 exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var myCalc = new Calculator();
+                Console.WriteLine(myCalc.Divide(5, 0));
+            }
+            // put an argument next to Exception so we can use the thrown exception in our msg
+            // put the most specific catches at the top and the most generic catches below
+            // note that these are all overloads with slightly different signatures
+            catch (System.DivideByZeroException ex)
+            {
+                Console.WriteLine("I'm pretty sure you can't " + ex.Message);
+            }
+            // most generic catch (catches System.Exception, the parent class of all exceptions)
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sh** just got f****d by " + ex.Message);
+                //throw new System.UnauthorizedAccessException().GetBaseException();
+            }
+        }
+    }
+}
